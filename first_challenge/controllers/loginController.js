256813677204082206c18aa0598254c7442dec39
strombach <fredde.stromback@gmail.com>
2025-05-25 14:23:49 +0200

    Adding simpler login logic

--- first_challenge/controllers/loginController.js
+++ first_challenge/controllers/loginController.js
@@ -1,11 +1,20 @@
 const loginController = {};
 
+const credentials = {
+    username: "admin",
+    password: "sUpeRsecRetanDrandoMpasswoRd"
+}
+
 loginController.index = (req, res) => {
     res.send(`<h2>Hello From LoginController</h2>`);
 };
 
 loginController.login = (req,res) => {
-    res.send(req.body)
+    if (req.body.username === credentials.username && req.body.password === credentials.password) {
+        res.send("Login Succesful")
+    } else {
+        res.send("Incorrect credentials")
+    }
 }
 
 module.exports = loginController;
